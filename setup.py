e36a24ee184d0b77a21ca46c489cd534db2dca55
Ramsi Kalia <ramsi.kalia@gmail.com>
2024-01-31 23:29:01 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multiclass classification of road signs using diverse methods, featuring from-scratch, transfer learning, and transformer architectures. Demonstrates model training and monitoring with DVC.',
+    author='ramsi-k',
+    license='',
+)
